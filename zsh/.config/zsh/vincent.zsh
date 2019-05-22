b9257c5c8231dbb1564ca369a0354c926200a4d1
angelo <angelo@fluxton.co.uk>
2019-05-22 02:11:32 +0100

    nearly fancy prompt

---
+++ zsh/.config/zsh/vincent.zsh
@@ -0,0 +1,108 @@
+## vincent
+# Create a hash table for globally stashing variables without polluting main
+# scope with a bunch of identifiers.
+typeset -A __WINCENT
+
+__WINCENT[ITALIC_ON]=$'\e[3m'
+__WINCENT[ITALIC_OFF]=$'\e[23m'
+
+autoload -U colors
+colors
+
+ RPROMPT_BASE="%F{blue}%~%f"
+ setopt PROMPT_SUBST
+
+
+zmodload zsh/parameter
+
+# and then in PS1: %(2v:<+%2v>:-)
+
+
+# Anonymous function to avoid leaking variables.
+function () {
+  # Check for tmux by looking at $TERM, because $TMUX won't be propagated to any
+  # nested sudo shells but $TERM will.
+  local TMUXING=$([[ "$TERM" =~ "tmux" ]] && echo tmux)
+  if [ -n "$TMUXING" -a -n "$TMUX" ]; then
+    # In a a tmux session created in a non-root or root shell.
+    local LVL=$(($SHLVL - 1))
+  else
+    # Either in a root shell created inside a non-root tmux session,
+    # or not in a tmux session.
+    local LVL=$SHLVL
+  fi
+  if [[ $EUID -eq 0 ]]; then
+    local SUFFIX='%F{yellow}%n%f'$(printf '%%F{yellow}\u276f%.0s%%f' {1..$LVL})
+  else
+    local SUFFIX=$(printf '%%F{red}\u276f%.0s%%f' {1..$LVL})
+  fi
+  export PS1="\n %(2v:<+%2v> %F{green}${SSH_TTY:+%n@%m}%f%B${SSH_TTY:+:}%b%F{blue}%B%1~%b%F{yellow}%B%(1j.*.)%(?..!)%b%f %B${SUFFIX}%b "
+  if [[ -n "$TMUXING" ]]; then
+    # Outside tmux, ZLE_RPROMPT_INDENT ends up eating the space after PS1, and
+    # prompt still gets corrupted even if we add an extra space to compensate.
+    export ZLE_RPROMPT_INDENT=0
+  fi
+}
+
+ export RPROMPT=$RPROMPT_BASE
+ export SPROMPT="zsh: correct %F{red}'%R'%f to %F{red}'%r'%f [%B%Uy%u%bes, %B%Un%u%bo, %B%Ue%u%bdit, %B%Ua%u%bbort]? "
+
+
+#
+# Hooks
+#
+
+autoload -U add-zsh-hook
+
+
+# # $HISTCMD (the current history event number) is shared across all shells
+# # (due to SHARE_HISTORY). Maintain this local variable to count the number of
+# # commands run in this specific shell.
+# HISTCMD_LOCAL=0
+
+
+# typeset -F SECONDS
+# function -record-start-time() {
+#   emulate -L zsh
+#   ZSH_START_TIME=${ZSH_START_TIME:-$SECONDS}
+# }
+# add-zsh-hook preexec -record-start-time
+
+function -report-start-time() {
+  emulate -L zsh
+  if [ $ZSH_START_TIME ]; then
+    local DELTA=$(($SECONDS - $ZSH_START_TIME))
+    local DAYS=$((~~($DELTA / 86400)))
+    local HOURS=$((~~(($DELTA - $DAYS * 86400) / 3600)))
+    local MINUTES=$((~~(($DELTA - $DAYS * 86400 - $HOURS * 3600) / 60)))
+    local SECS=$(($DELTA - $DAYS * 86400 - $HOURS * 3600 - $MINUTES * 60))
+    local ELAPSED=''
+    test "$DAYS" != '0' && ELAPSED="${DAYS}d"
+    test "$HOURS" != '0' && ELAPSED="${ELAPSED}${HOURS}h"
+    test "$MINUTES" != '0' && ELAPSED="${ELAPSED}${MINUTES}m"
+    if [ "$ELAPSED" = '' ]; then
+      SECS="$(print -f "%.2f" $SECS)s"
+    elif [ "$DAYS" != '0' ]; then
+      SECS=''
+    else
+      SECS="$((~~$SECS))s"
+    fi
+    ELAPSED="${ELAPSED}${SECS}"
+    export RPROMPT="%F{cyan}%{$__WINCENT[ITALIC_ON]%}${ELAPSED}%{$__WINCENT[ITALIC_OFF]%}%f $RPROMPT_BASE"
+    unset ZSH_START_TIME
+  else
+     export RPROMPT="$RPROMPT_BASE"
+  fi
+}
+add-zsh-hook precmd -report-start-time
+
+
+
+
+# # Remember each command we run.
+# function -record-command() {
+#   __WINCENT[LAST_COMMAND]="$2"
+# }
+# add-zsh-hook preexec -record-command
+
+
